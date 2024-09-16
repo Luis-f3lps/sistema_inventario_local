347f538e98e6f646d30c8c1f3dae1854ed2628a5
Luis Gustavo B. Santiago <74880596+Luis-f3lps@users.noreply.github.com>
2024-09-16 16:13:34 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -12,7 +12,6 @@ const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const app = express();
 const port = process.env.PORT || 3001;
 
 let pool;
