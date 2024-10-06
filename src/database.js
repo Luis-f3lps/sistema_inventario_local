9ed353f62aca17537427f3fdf274c14246a824df
Luis Gustavo B. Santiago <74880596+Luis-f3lps@users.noreply.github.com>
2024-10-05 21:44:32 -0300

    Update database.js

--- src/database.js
+++ src/database.js
@@ -1,14 +1,16 @@
+// src/public/database.js
 import mysql from 'mysql2/promise';
 import dotenv from 'dotenv';
 
-dotenv.config(); // Carrega as variáveis de ambiente do arquivo .env
+// Carregar variáveis de ambiente do arquivo .env
+dotenv.config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'ytf.h.filess.io', // O host do banco de dados
-  user: process.env.DB_USER || 'sistemaLab_jethalfway', // Nome do usuário
-  password: process.env.DB_PASSWORD || 'c8e375b482c025245599e5abb70c2fda7acfeb33', // Senha do usuário
-  database: process.env.DB_NAME || 'sistemaLab_jethalfway', // Nome do banco de dados
-  port: process.env.DB_PORT || 3307, // Porta do banco de dados
+  host: process.env.DB_HOST, // O host do banco de dados
+  user: process.env.DB_USER, // Nome do usuário
+  password: process.env.DB_PASSWORD, // Senha do usuário
+  database: process.env.DB_NAME, // Nome do banco de dados
+  port: process.env.DB_PORT, // Porta do banco de dados
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 3
